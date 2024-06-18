1485f2e4352ff0962982b6c7ccfa0121e0f1ec4e
Gilbert Reid <greid@spartaglobal.com>
2024-06-18 15:44:26 +0100

    Adding Animal class

---
+++ AbstractClassesAndInterfaces/src/main/java/com/sparta/grr/Animal.java
@@ -0,0 +1,39 @@
+package com.sparta.grr;
+
+//abstract classes deal with state and behaviour
+//what something is and what it can do
+public class Animal {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Animal(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
