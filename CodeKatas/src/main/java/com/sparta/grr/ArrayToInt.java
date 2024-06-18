5240b6f2d63fb639336d76d4d095082b40e115c1
Gilbert Reid <greid@spartaglobal.com>
2024-06-18 16:51:51 +0100

    Week 2 First Code Kata Complete

---
+++ CodeKatas/src/main/java/com/sparta/grr/ArrayToInt.java
@@ -0,0 +1,20 @@
+package com.sparta.grr;
+
+public class ArrayToInt {
+    public static int getAsInt(int[] arrayOfInts){
+        if(arrayOfInts.length < 1){
+            return 0;
+        }
+        if(arrayOfInts.length == 1){
+            return arrayOfInts[0];
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int num : arrayOfInts){
+            builder.append(num);
+        }
+        return Integer.parseInt(builder.toString());
+
+    }
+
+
+}
---
+++ CodeKatas/src/test/java/com/sparta/grr/ArrayToIntTest.java
@@ -0,0 +1,65 @@
+package com.sparta.grr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArrayToIntTest {
+    //Should return a int when given an array of ints[]
+    //should return integers as an int in the order they were given
+
+    @Test
+    @DisplayName("Given an array of ints[] return int")
+    void givenArrayOfIntsReturnInt(){
+        //Arrange
+        int expectedOutput = 1;
+        int[] inputs = new int[]{1};
+        //Act
+        int actualOutput = ArrayToInt.getAsInt(inputs);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+
+    }
+
+    @Test
+    @DisplayName("Given an array of ints[] return as ints connected")
+    void givenArrayOfIntsReturnAsIntsConnected(){
+        //Arrange
+        int expectedOutput = 1245;
+        int[] inputs = new int[]{1,2,4,5};
+        //Act
+        int actualOutput = ArrayToInt.getAsInt(inputs);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+
+    }
+
+    @Test
+    @DisplayName("Given an empty array of ints[] return 0")
+    void givenEmptyArrayOfIntsReturnZero(){
+        //Arrange
+        int expectedOutput = 0;
+        int[] inputs = new int[]{};
+        //Act
+        int actualOutput = ArrayToInt.getAsInt(inputs);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+
+    }
+
+    @Test
+    @DisplayName("Given an array of ints[] starting with zero return an int with no leading 0")
+    void givenArrayBeginingWithZeroOfIntsReturnIntWithNoLeadingZero(){
+        //Arrange
+        int expectedOutput = 3456;
+        int[] inputs = new int[]{0,34,5,6};
+        //Act
+        int actualOutput = ArrayToInt.getAsInt(inputs);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+
+    }
+
+
+
+}
