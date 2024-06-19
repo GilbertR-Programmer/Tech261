63bb5153ca523ca7d53d63d679717f1edd4061ac
Gilbert Reid <greid@spartaglobal.com>
2024-06-19 16:36:07 +0100

    Finishing codeKata fro findTriplet

--- CodeKatas/src/main/java/com/sparta/grr/FindTriplet/FindTriplet.java
+++ CodeKatas/src/main/java/com/sparta/grr/FindTriplet/FindTriplet.java
@@ -1,4 +1,28 @@
 package com.sparta.grr.FindTriplet;
 
 public class FindTriplet {
+    public static boolean containsTriplet(int[] numberArray){
+        if(isNull(numberArray)){
+            return false;
+        }
+
+        int matchCount = 0;
+        for(int i = 1; i < numberArray.length; i++){
+            if(numberArray[i] == numberArray[i-1]){
+                matchCount++;
+                if(matchCount == 2){
+                    return true;
+                }
+            } else{
+                matchCount = 0;
+            }
+
+        }
+        return false;
+    }
+
+    private static boolean isNull(int[] arrayForChecking){
+        return arrayForChecking == null;
+    }
+
 }
--- CodeKatas/src/test/java/com/sparta/grr/FindTripletTest/FindTripletTest.java
+++ CodeKatas/src/test/java/com/sparta/grr/FindTripletTest/FindTripletTest.java
@@ -3,6 +3,7 @@ package com.sparta.grr.FindTripletTest;
 import com.sparta.grr.FindTriplet.FindTriplet;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -34,4 +35,68 @@ public class FindTripletTest {
                 Arguments.of(new int[]{1,15,7,6,4,3})
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("hasTripletTests")
+    @DisplayName("Given an array of ints[] with sequence of three matching numbers return true")
+    void givenArrayOfIntsWithTripletReturnTrue(int[] inputs, boolean expectedOutput){
+        //Arrange
+
+        //Act
+        boolean actualOutput = FindTriplet.containsTriplet(inputs);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+    }
+
+    private static Stream<Arguments> hasTripletTests() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,2,2},true),
+                Arguments.of(new int[]{1,1,1,3},true),
+                Arguments.of(new int[]{12,1,1,1,6,4,3},true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("hasNoTripletTests")
+    @DisplayName("Given an array of ints[] without sequence of three matching numbers return false")
+    void givenArrayOfIntsWithoutTripletReturnFalse(int[] inputs, boolean expectedOutput){
+        //Arrange
+
+        //Act
+        boolean actualOutput = FindTriplet.containsTriplet(inputs);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+    }
+
+    private static Stream<Arguments> hasNoTripletTests() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,4,2},false),
+                Arguments.of(new int[]{1,5,1,3},false),
+                Arguments.of(new int[]{12,1,6,1,6,4,6},false)
+        );
+    }
+
+    @Test
+    @DisplayName("Given an null return false")
+    void givenNullReturnFalse(){
+        //Arrange
+        boolean expectedOutput = false;
+        //Act
+        boolean actualOutput = FindTriplet.containsTriplet(null);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+    }
+
+    @Test
+    @DisplayName("Given an empty Array return false")
+    void givenEmptyArrayReturnFalse(){
+        //Arrange
+        boolean expectedOutput = false;
+        int[] input = new int[]{};
+        //Act
+        boolean actualOutput = FindTriplet.containsTriplet(input);
+        //Assert
+        Assertions.assertEquals(expectedOutput,actualOutput);
+    }
+
 }
