0c8fca0c29db6d7cec3f321a5be4508eea816ed8
Gilbert Reid <greid@spartaglobal.com>
2024-06-19 15:25:47 +0100

    Adding a third spartan example

--- EqualsAndHashcode/src/main/java/com/sparta/grr/App.java
+++ EqualsAndHashcode/src/main/java/com/sparta/grr/App.java
@@ -4,11 +4,15 @@ public class App {
     public static void main(String[] args) {
         Spartan spartanOne = new Spartan("Manish","Gadhvi");
         Spartan spartanTwo = new Spartan("Manish", "Gadhvi");
+        Spartan spartanThree = new Spartan("Bob", "Gadhvi");
 
         System.out.println(spartanOne == spartanTwo);
         System.out.println(spartanOne.equals(spartanTwo));
 
         System.out.println(spartanOne.hashCode());
         System.out.println(spartanTwo.hashCode());
+
+        System.out.println(spartanOne.equals(spartanThree));
+        System.out.println(spartanThree.hashCode());
     }
 }
