213d35d1596055a8628e21b95d0afd9ac7bc4cda
Gilbert Reid <greid@spartaglobal.com>
2024-06-19 15:28:20 +0100

    adding an example with null

--- EqualsAndHashcode/src/main/java/com/sparta/grr/App.java
+++ EqualsAndHashcode/src/main/java/com/sparta/grr/App.java
@@ -14,5 +14,7 @@ public class App {
 
         System.out.println(spartanOne.equals(spartanThree));
         System.out.println(spartanThree.hashCode());
+
+        System.out.println(spartanOne.equals(null));
     }
 }
