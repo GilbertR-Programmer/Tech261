8ca1bb42b8743b187eaa9c3a4812f3a2b886a532
Gilbert Reid <greid@spartaglobal.com>
2024-06-25 15:12:38 +0100

    mvp of singleton example

---
+++ DesignPatterns/src/main/java/com/sparta/grr/singleton/SingletonExample.java
@@ -0,0 +1,12 @@
+package com.sparta.grr.singleton;
+
+public class SingletonExample {
+    private static final SingletonExample SINGLETON_EXAMPLE = new SingletonExample();
+    private SingletonExample(){
+
+    }
+
+    public static SingletonExample getSingletonExample(){
+        return SINGLETON_EXAMPLE;
+    }
+}
