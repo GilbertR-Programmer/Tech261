ff15d22468a92409b9d8c6a0691c8079c1ab0f29
Gilbert Reid <greid@spartaglobal.com>
2024-08-14 11:12:06 +0100

    basic web security working

--- SecurityPractice/src/main/java/com/sparta/grr/securitypractice/config/MvcConfig.java
+++ SecurityPractice/src/main/java/com/sparta/grr/securitypractice/config/MvcConfig.java
@@ -1,17 +1,20 @@
 package com.sparta.grr.securitypractice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        //registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/welcome").setViewName("welcome");
         registry.addViewController("/").setViewName("welcome");
         registry.addViewController("/error").setViewName("error");
         registry.addViewController("/login").setViewName("login");
     }
+
 }
--- SecurityPractice/src/main/java/com/sparta/grr/securitypractice/config/WebSecurityConfig.java
+++ SecurityPractice/src/main/java/com/sparta/grr/securitypractice/config/WebSecurityConfig.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,10 +22,28 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests((requests) -> requests
-                .anyRequest().permitAll()
-        );
-
-
+                .requestMatchers("/", "/welcome").permitAll()
+                .anyRequest().authenticated()
+        ).formLogin((form) -> form
+                .loginPage("/login")
+                .permitAll()
+        ).logout(LogoutConfigurer::permitAll);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withUsername("user")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
