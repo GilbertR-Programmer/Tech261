83ecf02fa74e4c72ef2e4353da0932a4e188a6a3
Gilbert Reid <greid@spartaglobal.com>
2024-08-15 13:36:50 +0100

    Commenting out views, starting again with api security

---
+++ SecurityPractice/src/main/java/com/sparta/grr/securitypractice/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.grr.securitypractice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    
+}
--- SecurityPractice/src/main/java/com/sparta/grr/securitypractice/controllers/ApiController.java
+++ SecurityPractice/src/main/java/com/sparta/grr/securitypractice/controllers/ApiController.java
@@ -1,7 +1,33 @@
 package com.sparta.grr.securitypractice.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Get!";
+    }
+
+    @GetMapping("/test")
+    public String helloExtended() {
+        return "Hello version 2";
+    }
+
+    @PostMapping
+    public String helloPost() {
+        return "Hello Post!";
+    }
+
+    @PutMapping
+    public String helloPut() {
+        return "Hello Put!";
+    }
+
+    @DeleteMapping
+    public String helloDelete() {
+        return "Hello Delete!";
+    }
 }
